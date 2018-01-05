fa3df1d6c3e0a5df61ca87d307a59eb60540d4f4
TheBeachmaster <otienoken@outlook.com>
2018-01-05 13:56:43 +0300

    Added libraries

---
+++ lib/AfricasTalkingCloud/src/test/mega/test/test_main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <unity.h>
+#include <AfricasTalkingCloud.h>
+#include <SPI.h>
+#include <WiFi101.h>
+#ifdef UNIT_TEST
+
+#endif
+
+void test_client_init(void){
+    
+}
